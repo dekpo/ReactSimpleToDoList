31cf176ace5995aad6eec0d3b378129e9120043b
Dekpo <dekpo@me.com>
2021-05-31 16:04:01 +0200

    Created a AddTask component

---
+++ src/components/AddTask.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class AddTask extends React.Component{
+    render(){
+        return(
+            <section>
+                <h1 className="m-3">New Task</h1>
+                <div className="card m-3">
+                    <form className="card-body">
+                        <div className="form-group m-2">
+                            <label htmlFor="taskName">Task Name</label>
+                            <input className="form-control" type="text" name="taskName" id="taskName" required></input>
+                        </div>
+                        <button type="submit" className="btn btn-primary m-2">Add Task</button>
+                    </form>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default AddTask
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ToDoList from './ToDoList';
 import NavBar from './NavBar';
+import AddTask from './AddTask';
 
 class App extends React.Component {
     render() {
       return (
         <section>
           <ToDoList />
+          <AddTask />
           <NavBar />
         </section>
       )
